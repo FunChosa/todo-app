cb77f7894c5d87abca999f34df55bd28f8fe4f5b
FunChosa <gilazieva.00@mail.ru>
2024-11-22 16:15:44 +0300

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,19 @@ import {
 } from "firebase/firestore";
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  bg: `h-screen w-screen p-4 bg-[#444643]`,
+  container: `bg-[#dadbd3] max-w-[500px] w-full m-auto rounded-xl shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between items-center`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`,
+  input: `rounded-md p-3 w-full text-xl`,
+  button: `rounded-[50px] p-2 ml-2 bg-[#91b774] text-slate-100`,
+  loading: `text-center p-2 mt-2 text-xl text-gray-400`,
 };
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
   //create todo
   const createTodo = async (e) => {
@@ -36,24 +37,29 @@ function App() {
     });
     setInputValue("");
   };
+
   //read todos
   useEffect(() => {
+    setLoading(true);
     const q = query(collection(db, "todos"));
     const unsub = onSnapshot(q, (querySnapshot) => {
       let todosArr = [];
       querySnapshot.forEach((doc) => {
         todosArr.push({ ...doc.data(), id: doc.id });
       });
+      setLoading(false);
       setTodos(todosArr);
     });
     return () => unsub();
   }, []);
+
   //update todo
   const toggleComplete = async (todo) => {
     await updateDoc(doc(db, "todos", todo.id), {
       completed: !todo.completed,
     });
   };
+
   //delete todo
   const deleteTodo = async (todo) => {
     await deleteDoc(doc(db, "todos", todo.id));
@@ -62,7 +68,7 @@ function App() {
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <h3 className={style.heading}>Todo App</h3>
+        <h3 className={style.heading}>To-do List</h3>
         <form className={style.form} onSubmit={createTodo}>
           <input
             value={inputValue}
@@ -76,20 +82,19 @@ function App() {
           </button>
         </form>
         <ul>
-          {todos.map((todo, index) => (
-            <Todo
-              key={index}
-              todo={todo}
-              toggleComplete={toggleComplete}
-              deleteTodo={deleteTodo}
-            />
-          ))}
+          {loading ? (
+            <p className={style.loading}>{"Loading..."}</p>
+          ) : (
+            todos.map((todo, index) => (
+              <Todo
+                key={index}
+                todo={todo}
+                toggleComplete={toggleComplete}
+                deleteTodo={deleteTodo}
+              />
+            ))
+          )}
         </ul>
-        {todos.length < 1 ? (
-          <p className={style.count}>{"You have no todos"}</p>
-        ) : (
-          <p className={style.count}>{`You have ${todos.length} todos`}</p>
-        )}
       </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,27 +2,23 @@ import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const style = {
-  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
-  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+  li: `flex justify-between bg-[#cccec3] p-4 my-2 capitalize rounded-md font-semibold`,
+  liComplete: `flex justify-between bg-[#cccec3] p-4 my-2 capitalize rounded-md font-semibold line-through  `,
   row: `flex`,
-  text: `ml-2 cursor-pointer`,
-  textComplete: `ml-2 cursor-pointer line-through`,
+  text: `ml-2 cursor-pointer max-w-[35ch] overflow-hidden text-ellipsis`,
   button: `flex items-center cursor-pointer`,
 };
 
 function Todo({ todo, toggleComplete, deleteTodo }) {
   return (
-    <li className={todo.completed ? style.liComplete : style.li}>
+    <li className={style.li + " " + (todo.completed ? style.liComplete : "")}>
       <div className={style.row}>
         <input
           onChange={() => toggleComplete(todo)}
           type="checkbox"
           checked={todo.completed ? "checked" : ""}
         />
-        <p
-          className={todo.completed ? style.textComplete : style.text}
-          onClick={() => toggleComplete(todo)}
-        >
+        <p className={style.text} onClick={() => toggleComplete(todo)}>
           {todo.text}
         </p>
       </div>
